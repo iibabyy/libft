ba6720396e9b76757dcfc33aec665a3652bad92c
Idrissa <utileidrissa@gmail.com>
2024-05-30 21:55:48 +0200

    final

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ibaby <ibaby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 22:35:02 by ibaby             #+#    #+#             */
-/*   Updated: 2024/05/19 18:48:13 by ibaby            ###   ########.fr       */
+/*   Updated: 2024/05/30 17:01:44 by ibaby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 int			ft_atoi(const char *str);
-int			ft_isacii(char c);
+int			ft_isascii(char c);
 int			ft_isalnum(char c);
 int			ft_isalpha(char c);
 int			ft_isdigit(char c);
@@ -52,6 +52,7 @@ char		**ft_split(const char *s, char sep);
 char		*ft_itoa(int n);
 void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
+void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void		ft_putnbr_fd(int n, int fd);
 void		ft_putendl_fd(char *s, int fd);
@@ -59,6 +60,7 @@ t_list		*ft_lstnew(void *content);
 void		ft_lstadd_front(t_list **lst, t_list *new);
 int			ft_lstsize(t_list *lst);
 t_list		*ft_lstlast(t_list *lst);
+
 void		ft_lstadd_back(t_list **lst, t_list *new);
 void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstdelone(t_list *lst, void (*del)(void *));
