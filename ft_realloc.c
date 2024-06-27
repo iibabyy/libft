91cf1293d7b58c9f982b03d461c4678c2bc44c29
idrissa <utileidrissa@gmail.com>
2024-06-27 02:22:00 +0200

    fixed realloc

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: ibaby <ibaby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 02:14:13 by ibaby             #+#    #+#             */
-/*   Updated: 2024/06/27 02:19:07 by ibaby            ###   ########.fr       */
+/*   Updated: 2024/06/27 02:21:32 by ibaby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	*ft_realloc(void *buffer, int buffer_size, size_t size, size_t sizeof_type)
 	unsigned char	*ptr;
 
 	ptr = ft_calloc(size, sizeof_type);
-	ft_memcpy(ptr, buffer, buffer_size * sizeof_type);
-	ft_free(&buffer);
+	if (buffer != NULL)
+	{
+		ft_memcpy(ptr, buffer, buffer_size * sizeof_type);
+		ft_free(&buffer);
+	}
 	return (ptr);
 }
